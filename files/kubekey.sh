ce9a29b56ffbf963119615253908c48cf1039c7d
Chris Smithson <csmithson12345@yahoo.com>
2023-04-29 12:02:53 -0400

    update keys

--- files/kubekey.sh
+++ files/kubekey.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
